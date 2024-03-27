c5fcd9650e8413df7b6d6684c0458e15de6b651a
xanna6 <apiotrow96@gmail.com>
2024-03-27 22:08:54 +0100

    product model created

--- store/models.py
+++ store/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name

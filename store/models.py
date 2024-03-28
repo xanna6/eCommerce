aaf0539588e3ad8e6e2dcab82a943e7d62b37458
xanna6 <apiotrow96@gmail.com>
2024-03-28 13:19:55 +0100

    displaying products from database

--- store/models.py
+++ store/models.py
@@ -11,6 +11,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    objects = models.Manager()
 
     def __str__(self):
         return self.name
--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 
 def products(request):
-    return render(request, 'products.html')
+    product_list = Product.objects.all()
+    context = {'products': product_list}
+    return render(request, 'products.html', context)

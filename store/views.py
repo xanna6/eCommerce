ccf47395264600f8bf98c9c4a5bcb641c32ba2db
xanna6 <apiotrow96@gmail.com>
2024-05-17 21:28:58 +0200

    cart page added

--- store/views.py
+++ store/views.py
@@ -6,3 +6,7 @@ def products(request):
     product_list = Product.objects.all()
     context = {'products': product_list}
     return render(request, 'products.html', context)
+
+
+def cart(request):
+    return render(request, 'cart.html')

ec436cf4dd5ce2c078ccb0f59090f7ec291f34c8
xanna6 <apiotrow96@gmail.com>
2024-06-01 20:03:26 +0200

    adding items to order in database on addToCart button click

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.products, name="products"),
     path('cart/', views.cart, name="cart"),
-    path('checkout/', views.checkout, name="checkout")
+    path('checkout/', views.checkout, name="checkout"),
+    path('update_cart/', views.updateCart, name="update_cart")
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
+import json
+
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
 
 
 def products(request):
@@ -32,3 +35,25 @@ def checkout(request):
 
     context = {'items': items, 'order': order}
     return render(request, 'checkout.html', context)
+
+
+def updateCart(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('ProductId:', productId)
+    print('Action:', action)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, completed=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order,product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+
+    orderItem.save()
+
+    return JsonResponse('Item was added to cart', safe=False)

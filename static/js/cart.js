f0e211406d00cda1fbc60fd2f6e94665376109ad
xanna6 <apiotrow96@gmail.com>
2024-06-06 14:36:08 +0200

    storing cart in cookies

--- static/js/cart.js
+++ static/js/cart.js
@@ -8,11 +8,32 @@ for (var i = 0; i < updateBtns.length; i++) {
 
         console.log('USER:', user)
         if (user === 'AnonymousUser') {
-            console.log('Not logged in')
+            updateCookieCart(productId, action)
         } else {
             updateUserOrder(productId, action)
         }
     })
+}
+
+function updateCookieCart(productId, action) {
+    console.log('Not logged in')
+
+    if (action == 'add') {
+        if (cart[productId] == undefined) {
+            cart[productId] = {'quantity': 1}
+        } else {
+           cart[productId]['quantity'] += 1
+        }
+    }
+
+    if (action == 'remove') {
+        cart[productId]['quantity'] -= 1
+        if (cart[productId]['quantity'] <= 0) {
+            delete(cart[productId])
+        }
+    }
+
+    document.cookie = 'cart=' + JSON.stringify(cart) + ';domain;path=/';
 }
 
 function updateUserOrder(productId, action) {
